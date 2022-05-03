7597a5d8991e4a12e25ffc487f9f5eae183ac2f5
Adryan <adryanynfante@gmail.com>
2022-05-03 12:25:23 -0500

    endpoint  de app
    endpoint  para listar las tareas y repsonsables

--- src/main/java/co/com/sofka/todolistback/controllers/TareasController.java
+++ src/main/java/co/com/sofka/todolistback/controllers/TareasController.java
@@ -1,1 +1,17 @@
-|
+package co.com.sofka.todolistback.controllers;
+
+import co.com.sofka.todolistback.models.TareasModel;
+import co.com.sofka.todolistback.services.TareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+public class TareasController {
+    @Autowired
+    private TareaService service;
+
+    @GetMapping(value = "api/todos")
+    public Iterable<TareasModel> list() {
+        return service.list();
+    }
